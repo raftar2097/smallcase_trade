58832985f78e31053d556a789d91ea4820a39549
Rishab Agarwala <ragarwala2097@gmail.com>
2022-01-22 23:14:39 +0530

    added new changes

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const api = require('./server/api');
 const db = require('./server/db');
 
 //Configure .env
-// require('dotenv').config();
+require('dotenv').config();
 
 //Set port as process.env.PORT if it is present otherwise set it to 4000
 const port = process.env.PORT || 4000;
 
 //Initiate connection with database
 db.connect({
-    host: process.env.DB_HOST||"cluster0.y76rw.mongodb.net",
-    username: process.env.DB_USER||"raftar2097",
-    password: process.env.DB_PASS||"raftar2097",
-    database: process.env.DB_NAME||"myFirstDatabase"
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 }).then(() => {
     //Handle /api with the api middleware
     app.use('/api', api);
